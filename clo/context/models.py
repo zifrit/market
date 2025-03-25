db22b3358abbfe2db231a11c1c9a2cf15bb1b220
zifrit <zifrit@list.ru>
2025-03-25 15:16:31 +0300

    mixins

--- clo/context/models.py
+++ clo/context/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class CreateUpdateTimeMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего обновления')
+
+    class Meta:
+        abstract = True
+
+
+class DeleteTimeMixin(models.Model):
+    delete = models.DateTimeField(verbose_name='Время удаления', blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class TimeStampMixin(CreateUpdateTimeMixin, DeleteTimeMixin):
+    class Meta:
+        abstract = True
+
+
+class CreatorMixin(models.Model):
+    creator = models.ForeignKey('users.CustomUser',on_delete=models.PROTECT, verbose_name='Создатель',
+                                null=True, blank=True)
+    class Meta:
+        abstract = True
