3cd997f7a3b17507c1de30325225c5355bfda183
zifrit <zifrit@list.ru>
2025-03-26 19:08:00 +0300

    register models in admins

--- clo/shop/admin.py
+++ clo/shop/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
+from shop.models import Shop, Sizes, Colors, Brands, Product
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    readonly_fields = ("id",)
+    search_fields = ("name",)
+
+
+@admin.register(Sizes)
+class SizesAdmin(admin.ModelAdmin):
+    readonly_fields = ("id",)
+
+@admin.register(Colors)
+class ColorsAdmin(admin.ModelAdmin):
+    readonly_fields = ("id",)
+
+@admin.register(Brands)
+class BrandsAdmin(admin.ModelAdmin):
+    readonly_fields = ("id",)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    readonly_fields = ("id",)
+
--- clo/users/admin.py
+++ clo/users/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+from users.models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class AdminCustomUser(UserAdmin):
+    list_display = ['id',
+                    'username',
+                    'fio',
+                    'phone',
+                    'email',
+                    'delete',
+                    'is_active',
+                    'is_staff']
+    list_display_links = ['id', 'username']
+    list_editable = ['is_active',
+                     'is_staff']
+    ordering = ['id', 'username']
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'password')
+        }),
+        (_('Personal info'), {
+            'fields': ('first_name', 'last_name', 'middle_name', 'email', 'phone',)
+        }),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'delete', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {
+            'fields': ('last_login', 'date_joined',)
+        }),
+    )
