97f4f0e15d0c2c664249c727f1e2e4e7db2ace1f
zifrit <zifrit@list.ru>
2025-04-11 14:31:39 +0300

    fix create upload photo for products

--- shop/api/images.py
+++ shop/api/images.py
@@ -6,48 +6,59 @@ from shop.models import ProductImages
 from rest_framework.viewsets import ModelViewSet
 from shop.api.serializers import ProductImagesSerializers
 
+
 class ProductImagesViewSet(generics.GenericAPIView):
     queryset = ProductImages.objects.all()
     serializer_class = ProductImagesSerializers
 
     @extend_schema(
         parameters=[
-            OpenApiParameter(name='product', description='Product identifier', required=True, type=int),
-            OpenApiParameter(name='color', description='Color identifier', required=True, type=int),
+            OpenApiParameter(
+                name="product",
+                description="Product identifier",
+                required=True,
+                type=int,
+            ),
+            OpenApiParameter(
+                name="color", description="Color identifier", required=True, type=int
+            ),
         ],
         request={
-            'multipart/form-data': {
-                'type': 'object',
-                'properties': {
-                    'file': {
-                        'type': 'string',
-                        'format': 'binary',
-                        'description': 'Файл для загрузки'
+            "multipart/form-data": {
+                "type": "object",
+                "properties": {
+                    "file": {
+                        "type": "string",
+                        "format": "binary",
+                        "description": "Файл для загрузки",
                     }
                 },
-                'required': ['file']
+                "required": ["file"],
             }
         },
         responses={
             201: {
-                'type': 'object',
-                'properties': {
-                    'message': {'type': 'string'},
-                    'filename': {'type': 'string'},
-                    'size': {'type': 'integer'}
-                }
+                "type": "object",
+                "properties": {
+                    "message": {"type": "string"},
+                    "filename": {"type": "string"},
+                    "size": {"type": "integer"},
+                },
             },
         },
-        summary='Загрузка фото продукта',
+        summary="Загрузка фото продукта",
     )
     def post(self, request, *args, **kwargs):
         for name, file in request.FILES.items():
-            data = request.data
+            data = request.query_params
             image = ProductImages.objects.create(
                 image=file,
                 name=file.name,
-                product_id=data.get('product'),
-                color_id=data.get('color'),
+                product_id=data.get("product"),
+                color_id=data.get("color"),
             )
-            return Response(data=ProductImagesSerializers(instance=image).data(),status=status.HTTP_201_CREATED)
+            return Response(
+                data=ProductImagesSerializers(instance=image).data,
+                status=status.HTTP_201_CREATED,
+            )
         return Response(status=status.HTTP_400_BAD_REQUEST)
