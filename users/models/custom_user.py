4d2f57ff87cf6385beb0727cdd035c4d4091fd96
zifrit <zifrit@list.ru>
2025-04-14 20:29:03 +0300

    added birthday and city in UserData model

--- users/models/custom_user.py
+++ users/models/custom_user.py
@@ -3,6 +3,7 @@ from django.db import models
 from context.models import TimeStampMixin
 from storages.backends.s3boto3 import S3Boto3Storage
 
+
 class CustomUserManager(UserManager):
 
     def create_user(self, phone_number, password=None, **kwargs):
@@ -23,20 +24,20 @@ class CustomUserManager(UserManager):
 class CustomUser(AbstractUser, TimeStampMixin):
     """Расширенный базовый пользователь"""
 
-    first_name = models.CharField(max_length=255, verbose_name='Фамилия', blank=True)
-    last_name = models.CharField(max_length=255, verbose_name='Имя', blank=True)
-    middle_name = models.CharField(max_length=255, verbose_name='Отчество', blank=True)
-    fio = models.CharField(max_length=255, verbose_name='ФИО', blank=True)
-    email = models.EmailField(verbose_name='Майл', unique=True,blank=True, null=True)
-    phone = models.CharField(max_length=18, unique=True, verbose_name='Номер телефона')
+    first_name = models.CharField(max_length=255, verbose_name="Фамилия", blank=True)
+    last_name = models.CharField(max_length=255, verbose_name="Имя", blank=True)
+    middle_name = models.CharField(max_length=255, verbose_name="Отчество", blank=True)
+    fio = models.CharField(max_length=255, verbose_name="ФИО", blank=True)
+    email = models.EmailField(verbose_name="Майл", unique=True, blank=True, null=True)
+    phone = models.CharField(max_length=18, unique=True, verbose_name="Номер телефона")
 
     objects = CustomUserManager()
 
     class Meta:
-        ordering = ['id']
-        db_table = 'custom_user'
-        verbose_name = 'CustomUser'
-        verbose_name_plural = 'CustomUsers'
+        ordering = ["id"]
+        db_table = "custom_user"
+        verbose_name = "CustomUser"
+        verbose_name_plural = "CustomUsers"
 
     def get_full_name(self):
         full_name = "%s %s %s" % (self.first_name, self.last_name, self.middle_name)
@@ -47,20 +48,30 @@ class CustomUser(AbstractUser, TimeStampMixin):
             return self.get_full_name()
         return self.username
 
+
 def user_image_upload_path(instance, filename):
     return f"users/{instance.user.first_name}-{instance.user.last_name}-{instance.gender}-{instance.age}/{filename}"
 
 
 class UserData(TimeStampMixin):
     class GenderType(models.TextChoices):
-        MALE = 'MALE', 'Мужчина'
-        FEMALE = 'FEMALE', 'Женщина'
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, verbose_name='Пользователь')
-    gender = models.CharField(max_length=10, choices=GenderType.choices, blank=True, null=True)
-    age = models.PositiveSmallIntegerField(verbose_name='Возраст', blank=True, null=True)
+        MALE = "MALE", "Мужчина"
+        FEMALE = "FEMALE", "Женщина"
+
+    user = models.OneToOneField(
+        CustomUser, on_delete=models.CASCADE, verbose_name="Пользователь"
+    )
+    gender = models.CharField(
+        max_length=10, choices=GenderType.choices, blank=True, null=True
+    )
+    age = models.PositiveSmallIntegerField(
+        verbose_name="Возраст", blank=True, null=True
+    )
+    birthday = models.DateTimeField(verbose_name="Дата рождения", blank=True, null=True)
+    city = models.CharField(verbose_name="Город", max_length=255, blank=True, null=True)
     image = models.ImageField(
         upload_to=user_image_upload_path,  # путь в S3
         storage=S3Boto3Storage(),
         null=True,
-        blank=True
+        blank=True,
     )
