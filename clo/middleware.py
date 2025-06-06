c3811d11604c5abe9afd8fe624123a38b73249cf
zifrit <zifrit@list.ru>
2025-06-06 14:57:38 +0300

    fix middleware

--- clo/middleware.py
+++ clo/middleware.py
@@ -1,13 +1,19 @@
+from django.utils.functional import SimpleLazyObject
+
+
 class UserGroupsMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        if hasattr(request, "user") and request.user.is_authenticated:
-            user_groups = list(request.user.groups.values_list("name", flat=True))
-            request.user_groups_name = user_groups
-        else:
-            request.user_groups_name = []
-
+        request.user_groups_name = SimpleLazyObject(
+            lambda: get_user_groups(request.user)
+        )
         response = self.get_response(request)
         return response
+
+
+def get_user_groups(user):
+    if user.is_authenticated:
+        return list(user.groups.values_list("name", flat=True))
+    return []
--- clo/settings.py
+++ clo/settings.py
@@ -240,7 +240,7 @@ REST_FRAMEWORK = {
     # регистрация идет только по токенам для проверки просто по логину паролю нужно закомитеть
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-        "rest_framework.authentication.BasicAuthentication",
+        # "rest_framework.authentication.BasicAuthentication",
         # "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": [
